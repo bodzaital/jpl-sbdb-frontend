97c4c8f2c28bb684d43cde0eaf65c41a42fa0fc6
Zsolt Boda <boda0zsolt@live.com>
2019-01-22 20:17:18 +0100

    Tooltip and nicer font

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function ToClipboard(s) {
 let url = new URL(window.location.href);
 let sstr = url.searchParams.get("sstr");
 
-let api = "https://ssd-api.jpl.nasa.gov/sbdb.api?sstr=" + sstr + "&orbit-defs=true";
+let api = "https://ssd-api.jpl.nasa.gov/sbdb.api?sstr=" + sstr + "&alt-des=true&alt-spk=true&phys-par=true";
 
 let ajax = new XMLHttpRequest();
 ajax.open("GET", api, true);
@@ -24,7 +24,16 @@ ajax.send();
 
 ajax.onload = () => {
 	if (ajax.readyState === ajax.DONE && ajax.status === 200) {
-		Render(JSON.parse(ajax.response));
+		let resp = JSON.parse(ajax.response);
+		if (resp.message != "specified object was not found") {
+			if (resp.signature.version == "1.1") {
+				Render(JSON.parse(ajax.response));
+			} else {
+				console.log("API version mismatch.");
+			}
+		} else {
+			console.log("Object not found.");
+		}
 	}
 };
 
--- render.js
+++ render.js
@@ -3,11 +3,11 @@ function Render(e)
 	console.log(e);
 
 	if (e.object.shortname === undefined) {
-		console.log("Doesn't have shortname.");
-		x("#object-shortname").innerText = e.object.fullname;
+		x("#object-name").innerText = e.object.fullname;
+		x("#object-name").dataset.tooltip = "Object doesn't have a longer name";
 	} else {
-		x("#object-shortname").innerText = e.object.shortname;
-		x("#object-fullname").innerText = " — " + e.object.fullname;
+		x("#object-name").innerText = e.object.shortname;
+		x("#object-name").dataset.tooltip = e.object.fullname;
 	}
 
 	x("#object-orbit").innerText = `${e.object.orbit_class.name} (${e.object.orbit_class.code})`;
